ccb00fd5e34c04c70828d5d16abd7488dcfb96dd
KSurfM <karlomarkovic@hotmail.com>
2022-12-12 10:42:27 +0100

    Discovery page working

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,17 +1,25 @@
 import MovieItem from "./MovieItem";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, Fragment } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const MovieList = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([[], [], []]);
+
+  const genreList = [53, 18, 28];
+  let movieArray = [];
 
   const fetchMoviesHandler = useCallback(async () => {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/search/multi?api_key=d76141fc516005c4b21c33a7c4f13e2f&language=en-US&query=Star%20Wars&page=1&include_adult=true"
-    );
-    const data = await response.json();
+    movieArray = [];
 
-    setMovies(data.results);
+    for (const genre of genreList) {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=d76141fc516005c4b21c33a7c4f13e2f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=${genre}&with_watch_monetization_types=flatrate`
+      );
+      const data = await response.json();
+      movieArray.push(data.results);
+    }
+    setMovies(movieArray);
+    console.log(movieArray);
   }, []);
 
   useEffect(() => {
@@ -19,25 +27,65 @@ const MovieList = () => {
   }, [fetchMoviesHandler]);
 
   return (
-    <div className="container">
-      <h2 className="mt-3">Star Wars</h2>
-      <div className="d-flex flex-row flex-nowrap overflow-auto">
-        {movies.map((movie) => (
-          <div
-            key={movie.id}
-            className="card card-block mx-2"
-            style={{ minWidth: "280px" }}
-          >
-            <MovieItem
+    <Fragment>
+      <div className="container">
+        <h2 className="mt-3">Thriller</h2>
+        <div className="d-flex flex-row flex-nowrap overflow-auto">
+          {movies[0].map((movie) => (
+            <div
               key={movie.id}
-              title={movie.original_title}
-              poster={movie.poster_path}
-              overview={movie.overview}
-            />
-          </div>
-        ))}
+              className="card card-block mx-2"
+              style={{ minWidth: "280px" }}
+            >
+              <MovieItem
+                key={movie.id}
+                title={movie.title}
+                poster={movie.poster_path}
+                overview={movie.overview}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="container">
+        <h2 className="mt-3">Drama</h2>
+        <div className="d-flex flex-row flex-nowrap overflow-auto">
+          {movies[1].map((movie) => (
+            <div
+              key={movie.id}
+              className="card card-block mx-2"
+              style={{ minWidth: "280px" }}
+            >
+              <MovieItem
+                key={movie.id}
+                title={movie.original_title}
+                poster={movie.poster_path}
+                overview={movie.overview}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="container">
+        <h2 className="mt-3">Action</h2>
+        <div className="d-flex flex-row flex-nowrap overflow-auto">
+          {movies[2].map((movie) => (
+            <div
+              key={movie.id}
+              className="card card-block mx-2"
+              style={{ minWidth: "280px" }}
+            >
+              <MovieItem
+                key={movie.id}
+                title={movie.original_title}
+                poster={movie.poster_path}
+                overview={movie.overview}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
