eecb8e08a73aca69799bf47c3cbebc2e3d97b526
KSurfM <karlomarkovic@hotmail.com>
2024-12-30 11:59:26 +0100

    edit constants

--- src/store/constants.ts
+++ src/store/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_IMG_URL = "https://image.tmdb.org/t/p/original";
-export const API_KEY = "992ccb70b3d864c718774b8841a13e44";
-export const BASE_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "4ccab08b7cbeddc8172073530a894394";
+export const BASE_URL = "https://api.themoviedb.org/3";
